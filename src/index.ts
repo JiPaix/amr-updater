a8eae26247d2b63327b25acb70230335350eaa2b
JiPaix <jipai13@gmail.com>
2021-09-06 13:01:29 +0000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as fs from 'fs'
+import * as from 'path'
+import * as stream from 'stream'
+import * as https from 'https'
+import dialog from 'dialog'
+// https://github.com/tomas/dialog#readme
