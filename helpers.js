eea10844e4ad422b75848ce881061d20f8e44674
Jean-Philippe ALLEGRO <jipai13@gmail.com>
2021-09-08 09:39:17 +0200

    checkManifest only checks the name
    manifest.json isn't updated when AMR has new commit so we need another way to check if its the latest version

--- helpers.js
+++ helpers.js
@@ -6,19 +6,29 @@ const {copy} = require('fs-extra')
 const AdmZip = require('adm-zip')
 
 exports.checkManifest = async (manifestPath) => {
-  
   const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'))
   return new Promise(async(resolve, reject) => {
     if(manifest.name.toLocaleLowerCase() !== 'all mangas reader') {
       reject('This is not AMR!')
     } else {
-      const res = await axios('https://raw.githubusercontent.com/JiPaix/AMR-BUILT/main/dist/manifest.json')
-      if(res.data.version === manifest.version) {
-        reject('Already up-to-date')
-      }
       resolve()
     }
   })
+}
+
+exports.checkVersion = async (dir) => {
+  const last_commitPath = path.join(dir, 'last_commit.txt')
+  if(fs.existsSync(last_commitPath)) {
+    const remote_commit = await axios('https://raw.githubusercontent.com/JiPaix/AMR-BUILT/main/dist/last_commit.txt')
+    const local_commit = fs.readFileSync(last_commitPath, 'utf-8')
+    if(remote_commit === local_commit) {
+      reject('already up-to-date')
+    } else {
+      resolve()
+    }
+  } else {
+    resolve() 
+  }
 }
 
 exports.clean = (folder, preinstall = true) => {
